0af3e2ed2714a696e512e48a7a83e8dda424e799
jadsonkenard <155968550+jadsonkenard@users.noreply.github.com>
2024-01-24 21:58:46 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,19 +2,28 @@ const botaoTarefas = document.querySelector('.task-button')
 const valorTarefas = document.querySelector('.task-input')
 const tarefasUl = document.querySelector('.task-ul')
 const tarefaFinalizada = document.querySelector('.task-ul-checked')
+
 botaoTarefas.addEventListener('click', addNovaTarefa)
+
 minhaListaTarefas = []
 tarefasConcluidas = []
 
-
-valorTarefas.addEventListener('keypress', function(e) {
-    if (e.keyCode === 13){
+valorTarefas.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13) {
         addNovaTarefa()
+
     }
 })
-
-
 function addNovaTarefa() {
+    if (valorTarefas.value.length >= 21) {
+        Swal.fire({
+            icon: "warning",
+            title: "Max: 20 caracteres",
+            confirmButtonColor: "#f00",
+        })
+        valorTarefas.value = valorTarefas.value
+        return
+    }
     if (!valorTarefas.value) {
         Swal.fire({
             icon: "warning",
@@ -22,8 +31,6 @@ function addNovaTarefa() {
             confirmButtonColor: "#f00",
         })
         return
-
-
     }
     if (minhaListaTarefas.includes(valorTarefas.value)) {
         Swal.fire({
@@ -45,17 +52,15 @@ function mostraTarefas() {
     minhaListaTarefas.forEach((tarefa, indice) => {
         novaTarefa = novaTarefa + `
             <li class="task-li">
-                <img src="assets/img/check.png" alt="Concluída" class="img-check" title="Concluída" onclick="concluirTarefa(${indice})">
-                <img src="assets/img/edit.png" alt="Editar" class="img-edit" title="Editar (Função indisponível)">
-                <img src="assets/img/del.png" alt="Apagar" class="img-del" title="Apagar" onclick="deletarTarefa(${indice})">
+                <img src="img/check.png" alt="Concluída" class="img-check" title="Concluída" onclick="concluirTarefa(${indice})">
                 <p>${tarefa}</p>
+                <img src="img/edit.png" alt="Editar" class="img-edit" title="Editar (Função indisponível)">
+                <img src="img/del.png" alt="Apagar" class="img-del" title="Apagar" onclick="deletarTarefa(${indice})">
             </li>
         `
     })
     tarefasUl.innerHTML = novaTarefa
 }
-
-
 function deletarTarefa(i) {
     Swal.fire({
         icon: "error",
@@ -66,11 +71,6 @@ function deletarTarefa(i) {
     }).then((result) => {
         if (result.isConfirmed) {
             confirmacaoDelete(i)
-            Swal.fire({
-                text: "Tarefa apagada",
-                icon: "success",
-                confirmButtonColor: "#3085d6",
-            })
         }
     })
 }
@@ -78,17 +78,13 @@ function confirmacaoDelete(i) {
     minhaListaTarefas.splice(i, 1)
     mostraTarefas()
 }
-
-
-function concluirTarefa(i){
+function concluirTarefa(i) {
     tarefasConcluidas.push(minhaListaTarefas[i])
     mostrarTarefasConcluidas()
     confirmacaoDelete(i)
     deleteTempo()
 }
-
-
-function mostrarTarefasConcluidas(){
+function mostrarTarefasConcluidas() {
     let concluida = ''
     tarefasConcluidas.forEach((tarefa, indice) => {
         concluida = concluida + `
@@ -99,18 +95,12 @@ function mostrarTarefasConcluidas(){
     })
     tarefaFinalizada.innerHTML = concluida
 }
-
-
-function deleteConcluidas(i){
+function deleteConcluidas(i) {
     tarefasConcluidas.splice(i, 1)
     mostrarTarefasConcluidas()
-
-
 }
-
-
 function deleteTempo() {
-    setTimeout(function(){
-    deleteConcluidas()
-  }, 15000)
+    setTimeout(function () {
+        deleteConcluidas()
+    }, 20000)
 }
